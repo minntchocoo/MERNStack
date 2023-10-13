d5b29d404dc43904f1a23e741641ff17b97069c3
minntchocoo <nicolettebridgette@gmail.com>
2023-10-13 18:27:59 +0800

    tutorial 6
    10:11 timestamp

---
+++ backend/controllers/itemController.js
@@ -0,0 +1,47 @@
+const Item = require('../models/itemModel')
+
+// get all items
+const getItems = async (req, res) => {
+    const items = await Item.find({}).sort({createdAt: -1})
+
+    res.status(200).json(items)
+}
+
+// get a single item
+const getItem = async (req, res) => {
+    const {id} = req.params
+
+    const item = await Item.findById(id)
+
+    // an error message is shown if an item does not exist
+    if (!item) {
+        return res.status(404).json({error: 'Item not found'})
+    }
+
+    res.status(200).json(item)
+}
+
+// create a new item
+const createItem = async (req, res) => {
+    const{name, variation, count} = req.body
+    
+    // add doc to db
+    try{
+        const item = await Item.create({name, variation, count})
+        res.status(200).json(item)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+
+    }
+}
+
+// delete an item 
+
+// update an item
+
+
+module.exports = {
+    getItems,
+    getItem,
+    createItem
+}
--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,31 +1,21 @@
 const express = require('express')
-const Item = require('../models/itemModel')
+const{
+    createItem,
+    getItems,
+    getItem
+} = require('../controllers/itemController')
 
 const router = express.Router()
 
 
 // to GET all ITEMS
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all items'})
-})
+router.get('/', getItems)
 
 // to GET a single ITEM
-router.get('/:id', (req, res) => {
-    res.json({mssg: ' GET a single item'})
-})
+router.get('/:id', getItem)
 
 // to POST a new ITEM
-router.post('/', async (req, res) => {
-    const{name, variation, count} = req.body
-
-    try{
-        const item = await Item.create({name, variation, count})
-        res.status(200).json(item)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-
-    }
-})
+router.post('/', createItem)
 
 // to DELETE an ITEM
 router.delete('/:id',(req, res) => {
